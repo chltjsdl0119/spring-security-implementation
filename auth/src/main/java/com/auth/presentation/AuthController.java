6447a2da72891c0d7637211b050060adebf331f3
Seungwon-Choi <bestwondev@gmail.com>
2025-08-29 20:00:05 +0900

    feat: auth 모듈 인증 기능 추가

---
+++ auth/src/main/java/com/auth/presentation/AuthController.java
@@ -0,0 +1,36 @@
+package com.auth.presentation;
+
+import com.auth.application.UserService;
+import com.auth.domain.Otp;
+import com.auth.domain.User;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/user/add")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @PostMapping("/user/auth")
+    public void auth(@RequestBody User user) {
+        userService.auth(user);
+    }
+
+    @PostMapping("/otp/check")
+    public void check(@RequestBody Otp otp, HttpServletResponse response) {
+        if (userService.check(otp)) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+}
---
+++ auth/src/main/java/com/auth/util/GenerateCodeUtil.java
@@ -0,0 +1,27 @@
+package com.auth.util;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public final class GenerateCodeUtil {
+
+    private GenerateCodeUtil() {
+    }
+
+    public static String generateCode() {
+        String code;
+
+        try {
+            SecureRandom random = SecureRandom.getInstanceStrong();
+
+            int c = random.nextInt(9000) + 1000;
+
+            code = String.valueOf(c);
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        return code;
+    }
+}
